b6887027b7b483adb49aa5335b0267510b6e91fc
ashrouge <raflyputra0510@gmail.com>
2023-11-22 22:01:29 +0700

    next

--- src/common/widgets/Footer/Footer.tsx
+++ src/common/widgets/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <section id="__footer" className='py-5' >
        <div className='text-center text-sm text-slate-400' >
 
-          <p className='opacity-90' >&copy; 2023, Built by <a href='https://github.com/rasyidridho05' target='_blank' className='text-sky-400' >@Rasyid Ridho</a> </p>
+          <p className='opacity-90' >&copy; 2023, Built by <a href='https://github.com/ashrouge' target='_blank' className='text-sky-400' >@ashrouge</a> </p>
 
        </div>
     </section>
