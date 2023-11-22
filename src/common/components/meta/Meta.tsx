bd57d443e6958bad4eb89d7341f6078c56ff2210
ashrouge <raflyputra0510@gmail.com>
2023-11-22 21:49:06 +0700

    next

--- src/common/components/meta/Meta.tsx
+++ src/common/components/meta/Meta.tsx
@@ -5,7 +5,7 @@ const Meta = () => {
       <link rel="canonical" href="https://www.rasyidridho.my.id/"></link>
       <link rel="pingback" href="https://www.rasyidridho.my.id/"></link>
 
-      <title> Muhammad Rasyid Ridho</title>
+      <title> Johar Murjani</title>
 
       <meta name="description" content='Highly motivated and passionate Graphic Design and Front End Development with one years expereince in Adobe Illustrator, Adobe Photoshop, Figma, React js, Next js, Tailwind CSS, Node js, MySQL' />
       <meta
